15167c745b2a449b2ec926bf3538c2efdfa8f273
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2025-02-24 17:12:23 +0700

    refactor: organize code (#2)

--- src/routes/v1/wallet.ts
+++ src/routes/v1/wallet.ts
@@ -1,28 +1,11 @@
 import { Router } from "express";
-import { lucidService } from "@/services/lucid";
-import { CustomExpress } from "@/pkg/app/response";
+import { walletController } from "@/controllers";
 
 const router: Router = Router();
 
-router.post("/private-key", async (req, res, next) => {
-    const appExpress = new CustomExpress(req, res, next);
+router.post("/private-key", walletController.generatePrivateKey);
 
-    const privateKey = await lucidService.generatePrivateKey();
-
-    appExpress.response201({
-        privateKey
-    });
-})
-
-router.post("/seed-phrase", async (req, res, next) => {
-    const appExpress = new CustomExpress(req, res, next);
-
-    const seedPhrase = await lucidService.generateSeedPhrase();
-
-    appExpress.response201({
-        seedPhrase
-    });
-})
+router.post("/seed-phrase", walletController.generateSeedPhrase);
 
 export default router;
 
