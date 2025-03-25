e0bc6cf33d4182f3f13d55925b19928ec30977e5
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2025-03-25 09:48:41 +0700

    feat: get transactions and detail of each transaction

--- src/types/api/blockforst/index.ts
+++ src/types/api/blockforst/index.ts
@@ -27,4 +27,36 @@ export interface AssetDetailsResponse {
     onchain_metadata_standard: string | null;
     onchain_metadata_extra: null;
     metadata: null;
-} 
+}
+
+export interface TransactionResponse {
+    tx_hash: string;
+    tx_index: number;
+    block_height: string;
+    block_time: number;
+}
+
+export interface TransactionDetailResponse {
+    hash: string;
+    block: string;
+    block_height: number;
+    block_time: number;
+    slot: number;
+    index: number;
+    output_amount: Amount[];
+    fees: string;
+    deposit: string;
+    size: number;
+    invalid_before: string | null;
+    invalid_hereafter: string;
+    utxo_count: number;
+    withdrawal_count: number;
+    mir_cert_count: number;
+    delegation_count: number;
+    stake_cert_count: number;
+    pool_update_count: number;
+    pool_retire_count: number;
+    asset_mint_or_burn_count: number;
+    redeemer_count: number;
+    valid_contract: boolean;
+}
